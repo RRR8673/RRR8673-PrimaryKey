1fb082ff65a10830c25a4695164989c2d67815a3
Rafael Renigado <renigado@aol.com>
2023-03-01 16:06:25 +0000

    done

---
+++ problem1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE song (
+    id SERIAL PRIMARY KEY, 
+    Title varchar(100),
+    Artist varchar(100)
+);
+INSERT INTO song VALUES('Let it be', 'Beatles');
+INSERT INTO song VALUES('Hotel California', 'Eagles');
+INSERT INTO song VALUES('Kashmir', 'Led Zeppelin');
